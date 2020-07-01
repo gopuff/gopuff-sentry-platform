12afaaff08d32352ec086c21609d226d29372c11
Chris Maenner <chris.maenner@gopuff.com>
2020-07-01 17:58:33 -0400

    Renamed dir

--- containers/slack/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-/usr/share/logstash/bin/logstash --pipeline.unsafe_shutdown --config.reload.automatic --path.settings /etc/logstash --path.data /var/feeds/azure/ --log.format json --log.level info
